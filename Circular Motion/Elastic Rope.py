ddddb7997746599a25cb327ffa9241ee9f28f87f
jpw447 <joepw1@hotmail.com>
2023-01-31 12:50:03 +0000

    Animation & Error Study

--- Circular Motion/Elastic Rope.py
+++ Circular Motion/Elastic Rope.py
@@ -9,6 +9,9 @@ change is shown in the third plot.
 When the radius returns to its initial value, the radial velocity also
 returns to its initial value.
 '''
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp, cumtrapz
 
 
 def spring_polar_soln(t, parameters):
@@ -26,15 +29,11 @@ def spring_polar_soln(t, parameters):
 
 
 if __name__ == "__main__":
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from scipy.integrate import solve_ivp, cumtrapz
-
     # Parameters and initial conditions
     m = 1  # Mass
     k = 1  # Spring constant
     L = 1  # Natural spring length
-    r_init = 1.1
+    r_init = 2
     E_init = 2*np.pi**2  # Chosen to make initial omega = 2pi
     w_init = np.sqrt(2*E_init/(m*r_init**2))
     v_t_init = w_init*r_init
@@ -45,7 +44,7 @@ if __name__ == "__main__":
     ic = [r_init, v_r_init, v_t_init, w_init, E_init]
 
     # Solution from 0 to t=2pi
-    soln = solve_ivp(spring_polar_soln, [0, 2*tmax], ic, method='RK23',
+    soln = solve_ivp(spring_polar_soln, [0, 2*tmax], ic, method='DOP853',
                      dense_output=True, rtol=1e-8)
 
     tvals = np.linspace(0, tmax, N_points)
@@ -99,3 +98,78 @@ if __name__ == "__main__":
     ax_pol[1].set_xlabel("$t$", fontsize=16)
     ax_pol[1].set_ylabel("$\\theta$", fontsize=16)
     ax_pol[1].set_title("$\\theta(t)$", fontsize=20)
+
+#%% Animating the dynamics
+from matplotlib.animation import FuncAnimation
+
+if __name__ == "__main__":
+    fig_ani, ax_ani = plt.subplots(subplot_kw={'projection': 'polar'})
+    x = []
+    y = []
+
+    def animate(i):
+        # xval = r[i]*np.cos(theta[i])
+        # yval = r[i]*np.sin(theta[i])
+        # x.append(xval)
+        # y.append(yval)
+        x.append(theta[i])
+        y.append(r[i])
+        ax_ani.clear()
+        ax_ani.plot(x, y)
+        ax_ani.set_ylim(0, 2)
+
+    ani = FuncAnimation(fig_ani, animate, frames=N_points, interval=100,
+                        repeat=False)
+    plt.show()
+
+#%% Error tolerance study
+'''
+Here, the impact of each solving method is demonstrated, along with the impact
+of the error. We can see that higher order corrections (DOP853) provide a
+higher resolution at lower errors. Low order solvers (RK23) still provide
+innaccurate results.
+
+Changing the number of points (N_points) changed the solution. This will be
+due to the close encounter around the origin for the given parameter (r_init=2)
+'''
+
+if __name__ == "__main__":
+    # Parameters and initial conditions
+    m = 1  # Mass
+    k = 1  # Spring constant
+    L = 1  # Natural spring length
+    r_init = 2
+    E_init = 2*np.pi**2  # Chosen to make initial omega = 2pi
+    w_init = np.sqrt(2*E_init/(m*r_init**2))
+    v_t_init = w_init*r_init
+    v_r_init = 0
+
+    N_points = 256
+    tmax = 2*np.pi
+    ic = [r_init, v_r_init, v_t_init, w_init, E_init]
+
+    error_list = np.logspace(-10, -6, 3)
+    fig_pol, ax_pol = plt.subplots(3, len(error_list), figsize=(12, 8),
+                                   subplot_kw={'projection': 'polar'})
+
+    for i, method in enumerate(["RK23", "RK45", "DOP853"]):
+        ax_pol[i][0].set_ylabel(method, rotation=0, labelpad=70, fontsize=20)
+
+        for j, error in enumerate(error_list):
+            # Solution from 0 to t=2pi
+            soln = solve_ivp(spring_polar_soln, [0, 2*tmax], ic, method=method,
+                             dense_output=True, rtol=error)
+
+            tvals = np.linspace(0, tmax, N_points)
+            soln = soln.sol(tvals)
+
+            # Variables from solution and energy variation
+            r = soln[0]
+            w = soln[3]
+
+            # Finding angle theta by integrating omega
+            theta = cumtrapz(w, tvals, initial=0)
+
+            ax_pol[i][j].plot(theta, r)
+            ax_pol[0][j].set_title(error, fontsize=20)
+    fig_pol.tight_layout()
